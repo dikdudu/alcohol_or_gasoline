3551d8d693165e0fd171004d98283cc7f5d39e84
Eduardo Santos <dikdudu@hotmail.com>
2020-11-16 15:24:18 -0300

    new icon

--- lib/app/components/logo.dart
+++ lib/app/components/logo.dart
@@ -5,12 +5,12 @@ class Logo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        SizedBox(height: 80),
+        SizedBox(height: 60),
         Image.asset(
-          'assets/images/logo.png',
+          'assets/images/icon.png',
           height: 80,
         ),
-        SizedBox(height: 30),
+        SizedBox(height: 50),
       ],
     );
   }
